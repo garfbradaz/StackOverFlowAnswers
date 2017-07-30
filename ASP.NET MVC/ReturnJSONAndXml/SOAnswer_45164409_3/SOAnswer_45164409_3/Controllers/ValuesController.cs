1cc51921a3622f72c9873d35297a48ee3e544988
Gareth Bradley <gareth.bradley@hachette.co.uk>
2017-07-30 14:44:32 +0100

    First commit

---
+++ ASP.NET MVC/ReturnJSONAndXml/SOAnswer_45164409_3/SOAnswer_45164409_3/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SOAnswer_45164409_3.Models;
+
+
+namespace SOAnswer_45164409_3.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    { 
+        // GET api/values
+        [HttpGet]
+        [FormatReponseFilter]
+        public IActionResult Get()
+        {
+            TestModel model = new TestModel { Id = 24, Name = "Gareth" };
+            return new ObjectResult(model);
+        }
+
+      
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASP.NET MVC/ReturnJSONAndXml/SOAnswer_45164409_3/SOAnswer_45164409_3/FormatReponseFilterAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml.Extensions;
+
+namespace SOAnswer_45164409_3
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class FormatReponseFilterAttribute : Attribute, IActionFilter
+    {
+        private enum FormatResponseType { Json, Xml, Unknown }
+        private FormatResponseType _requestedType { get; set; }
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var _result = (ObjectResult)filterContext.Result;
+            switch (_requestedType)
+            {
+
+                case FormatResponseType.Json:
+                    var data = new { Data = _result.Value };
+                    filterContext.Result = new JsonResult(data);
+                    break;
+                case FormatResponseType.Xml:
+                    filterContext.Result = new XmlResult(_result.Value);
+                    break;
+                case FormatResponseType.Unknown:
+                default:
+                    throw new InvalidOperationException("Uknown Content Type ain Accept Header");
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var _contentType = filterContext.HttpContext.Request.Headers["Accept"];
+
+            switch (_contentType)
+            {
+                case string s when (s.Contains("application/json")):
+                    _requestedType = FormatResponseType.Json;
+                    break;
+                case string e when (e.Contains("/xml")):
+                    _requestedType = FormatResponseType.Xml;
+                    break;
+                default:
+                    throw new ArgumentException("Unknown Accept Type - dont know how to handle: " + _contentType);
+            }
+        }
+    }
+}
+
